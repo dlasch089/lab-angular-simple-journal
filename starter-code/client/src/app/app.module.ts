048a3c49e196082589aee25907b3cf0689d9d3cd
Dominik Lasch <dl@exutec.de>
2017-11-30 22:41:37 +0100

    iteration 1 & 2

--- starter-code/client/src/app/app.module.ts
+++ starter-code/client/src/app/app.module.ts
@@ -2,19 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
+import { JournalEntriesService } from './services/journal-entries.service';
+
+import { EntryListComponent } from './components/entry-list/entry-list.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: EntryListComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntryListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    JournalEntriesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ starter-code/client/src/app/components/entry-list/entry-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+import { JournalEntriesService } from '../../services/journal-entries.service';
+import { JournalEntry } from '../../models/journalEntry';
+
+// import { DatePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-entry-list',
+  templateUrl: './entry-list.component.html',
+  styleUrls: ['./entry-list.component.css']
+})
+export class EntryListComponent implements OnInit {
+  journalEntryList: JournalEntry[];
+  journalEntry: JournalEntry;
+
+  constructor(private theJournalEntriesService: JournalEntriesService) { }
+
+  ngOnInit() {
+    this.theJournalEntriesService.getJournalEntries().subscribe((journalEntryList) => this.journalEntryList = journalEntryList);
+  }
+
+}
